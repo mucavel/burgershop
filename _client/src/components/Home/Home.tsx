2363d3ab68e93e72592d9048503b63856c5db577
mucavel <salvadormuca7@gmail.com>
2022-07-31 14:08:00 +0200

    Built Home component

---
+++ _client/src/components/Home/Home.tsx
@@ -0,0 +1,31 @@
+import './css/Home.css'
+import Navbar from './Navbar'
+const logo = require('../../assets/images/logo.png')
+
+const Home = () => {
+  return (
+    <header id='home'>
+        <Navbar/>
+        <div className="home">
+            <div className='home-text'>
+                <div className='home-text-container'>
+                    <h1 className='home-h1'>The Burger Shop</h1>
+                    <h2 className='home-h2'>O melhor Humbúrger tu encontras aqui!</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        labore obcaecati suscipit dolorum quis minima omnis libero quo aperiam esse.
+                    </p>
+                    <button className='home-by-btn'>
+                        <a href="#choose">Comprar</a>
+                    </button>
+                </div>
+            </div>
+            <div className="home-logo">
+                <img src={logo} alt="Humburger Logo" />
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Home
---
+++ _client/src/components/Home/Navbar.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import "./css/Navbar.css";
+import { FaHamburger } from 'react-icons/fa'
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="Navbar">
+      <span className="nav-logo"><FaHamburger className="burger-ico"/>Burger Shop</span>
+      <div className={`nav-items ${isOpen && "open"}`}>
+        <a href="#home">Home</a>
+        <a href="#menu">Menu</a>
+        <a href="#events">Promoções</a>
+        <a href="#book">Entregas</a>
+        <a href="#footer">Contacte-nos</a>
+      </div>
+      <div
+        className={`nav-toggle ${isOpen && "open"}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div className="bar"></div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
