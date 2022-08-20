1128e9b4b74c55b2ac1a88f658819f5036f6bcda
mucavel <salvadormuca7@gmail.com>
2022-08-20 16:38:54 +0200

    Input Validation

--- _client/src/components/Book/Book.tsx
+++ _client/src/components/Book/Book.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useRef } from 'react';
+import { MouseEvent, useEffect, useRef } from 'react';
+import Axios from 'axios';
 import SubTitle from '../SubTitle/SubTitle';
 import './css/Book.css';
 
@@ -16,72 +17,123 @@ const Book = ({burgerID}:ChoiceIDType) => {
     const hamburguer_duplo_acompanhado = useRef<HTMLOptionElement>(null);
     const batata_tomate_mayonaise = useRef<HTMLOptionElement>(null);
 
-    
+    const nameInput = useRef<HTMLInputElement>(null)
+    const telInput = useRef<HTMLInputElement>(null)
+    const addressInput = useRef<HTMLInputElement>(null)
+    const obsRef = useRef<HTMLTextAreaElement>(null)
+
     useEffect(() => {
         switch (burgerID) {
             case 1:
                 hamburguer_simples.current?.setAttribute('selected', 'selected')
-                hamburguer_duplo.current?.setAttribute('hidden', 'hidden')
-                hamburguer_duplo_king.current?.setAttribute('hidden', 'hidden')
-                batata_frita_com_tomate.current?.setAttribute('hidden', 'hidden')
-                hamburguer_duplo_acompanhado.current?.setAttribute('hidden', 'hidden')
-                batata_tomate_mayonaise.current?.setAttribute('hidden', 'hidden')
+                hamburguer_duplo.current?.removeAttribute('selected')
+                hamburguer_duplo_king.current?.removeAttribute('selected')
+                batata_frita_com_tomate.current?.removeAttribute('selected')
+                hamburguer_duplo_acompanhado.current?.removeAttribute('selected')
+                batata_tomate_mayonaise.current?.removeAttribute('selected')
                 break;
             case 2:
                 hamburguer_duplo.current?.setAttribute('selected', 'selected')
-                hamburguer_simples.current?.setAttribute('hidden', 'hidden')
-                hamburguer_duplo.current?.setAttribute('hidden', 'hidden')
-                hamburguer_duplo_king.current?.setAttribute('hidden', 'hidden')
-                batata_frita_com_tomate.current?.setAttribute('hidden', 'hidden')
-                hamburguer_duplo_acompanhado.current?.setAttribute('hidden', 'hidden')
-                batata_tomate_mayonaise.current?.setAttribute('hidden', 'hidden')
+                hamburguer_simples.current?.removeAttribute('selected')
+                hamburguer_duplo_king.current?.removeAttribute('selected')
+                batata_frita_com_tomate.current?.removeAttribute('selected')
+                hamburguer_duplo_acompanhado.current?.removeAttribute('selected')
+                batata_tomate_mayonaise.current?.removeAttribute('selected')
                 break;
             case 3:
                 hamburguer_duplo_king.current?.setAttribute('selected', 'selected')
-                hamburguer_simples.current?.setAttribute('hidden', 'hidden')
-                hamburguer_duplo.current?.setAttribute('hidden', 'hidden')
-                batata_frita_com_tomate.current?.setAttribute('hidden', 'hidden')
-                hamburguer_duplo_acompanhado.current?.setAttribute('hidden', 'hidden')
-                batata_tomate_mayonaise.current?.setAttribute('hidden', 'hidden')
+                hamburguer_simples.current?.removeAttribute('selected')
+                hamburguer_duplo.current?.removeAttribute('selected')
+                batata_frita_com_tomate.current?.removeAttribute('selected')
+                hamburguer_duplo_acompanhado.current?.removeAttribute('selected')
+                batata_tomate_mayonaise.current?.removeAttribute('selected')
                 break;
             case 4:
                 batata_frita_com_tomate.current?.setAttribute('selected', 'selected')
-                hamburguer_simples.current?.setAttribute('hidden', 'hidden')
-                hamburguer_duplo.current?.setAttribute('hidden', 'hidden')
-                hamburguer_duplo_king.current?.setAttribute('hidden', 'hidden')
-                hamburguer_duplo_acompanhado.current?.setAttribute('hidden', 'hidden')
-                batata_tomate_mayonaise.current?.setAttribute('hidden', 'hidden')
+                hamburguer_simples.current?.removeAttribute('selected')
+                hamburguer_duplo.current?.removeAttribute('selected')
+                hamburguer_duplo_king.current?.removeAttribute('selected')
+                hamburguer_duplo_acompanhado.current?.removeAttribute('selected')
+                batata_tomate_mayonaise.current?.removeAttribute('selected')
                 break;
             case 5:
                 hamburguer_duplo_acompanhado.current?.setAttribute('selected', 'selected')
-                hamburguer_simples.current?.setAttribute('hidden', 'hidden')
-                hamburguer_duplo.current?.setAttribute('hidden', 'hidden')
-                hamburguer_duplo_king.current?.setAttribute('hidden', 'hidden')
-                batata_frita_com_tomate.current?.setAttribute('hidden', 'hidden')
-                batata_tomate_mayonaise.current?.setAttribute('hidden', 'hidden')
+                hamburguer_simples.current?.removeAttribute('selected')
+                hamburguer_duplo.current?.removeAttribute('selected')
+                hamburguer_duplo_king.current?.removeAttribute('selected')
+                batata_frita_com_tomate.current?.removeAttribute('selected')
+                batata_tomate_mayonaise.current?.removeAttribute('selected')
                 break;
             case 6:
                 batata_tomate_mayonaise.current?.setAttribute('selected', 'selected')
-                hamburguer_simples.current?.setAttribute('hidden', 'hidden')
-                hamburguer_duplo.current?.setAttribute('hidden', 'hidden')
-                hamburguer_duplo_king.current?.setAttribute('hidden', 'hidden')
-                batata_frita_com_tomate.current?.setAttribute('hidden', 'hidden')
-                hamburguer_duplo_acompanhado.current?.setAttribute('hidden', 'hidden')
+                hamburguer_simples.current?.removeAttribute('selected')
+                hamburguer_duplo.current?.removeAttribute('selected')
+                hamburguer_duplo_king.current?.removeAttribute('selected')
+                batata_frita_com_tomate.current?.removeAttribute('selected')
+                hamburguer_duplo_acompanhado.current?.removeAttribute('selected')
                 break;
             default:
                 break;
         }
     }, [burgerID])
     
+    const book = (e:MouseEvent) => {
+        e.preventDefault();
+
+        if(
+            nameInput.current?.value === null ||
+            nameInput.current?.value === '' ||
+            telInput.current?.value === null ||
+            telInput.current?.value === '' ||
+            addressInput.current?.value === null ||
+            addressInput.current?.value === ''
+        ){
+            alert('Preencha todos Campos!')
+        }else if(
+            telInput.current?.value.substring(0, 2) === '82' ||
+            telInput.current?.value.substring(0,2) === '83' ||
+            telInput.current?.value.substring(0, 2) === '84' ||
+            telInput.current?.value.substring(0, 2)  === '85' ||
+            telInput.current?.value.substring(0, 2)  === '86' ||
+            telInput.current?.value.substring(0, 2) === '87'
+        ){
+            if(telInput.current?.value.length !== 9){
+                alert('Número de Telefone incompleto')
+            }else{
+                Axios.post("http://localhost:3001/booking", {
+                    buyerName : nameInput.current?.value,
+                    buyerTel : telInput.current?.value,
+                    buyerAddress : addressInput.current?.value,
+                    buyerChoice : selectRef.current?.value,
+                    observations : obsRef.current?.value
+                }).then((response) => {
+                    if(response.status === 200 && response.data.affectedRows === 1){
+                        alert('Pedido feito com Sucesso!')
+                        window.location.reload()
+                    }else{
+                        alert('Ocorreu um erro. Verifique seus dados e tente novamente.')
+                    }
+                })
+            }
+        }else{
+            alert('Verifique o número de Telefone')
+        }
+
+
+
+    }
+
     return (
         <section id="book">
             <SubTitle subtitle='Faça sua Encomenda'/>
             <div className="form-container">
-                <form id="form" action="https://formsubmit.co/47e82810e0ba444668143cbcdc232385" method='POST'>
+                <form id="form">
+                {/* <form id="form" action="https://formsubmit.co/47e82810e0ba444668143cbcdc232385" method='POST'> */}
+
                     <div className="form personal-info">
-                        <input type="text" placeholder='Seu Nome' required name='name' autoComplete='off'maxLength={30}/>
-                        <input type="tel" placeholder="Seu Telefone (+258)" required name='tel' autoComplete='off' maxLength={9}/>
-                        <input type="text" placeholder='Seu Endereço' required name='address' autoComplete='off' maxLength={100}/>
+                        <input type="text" ref={nameInput} placeholder='Seu Nome' required name='name' autoComplete='off' maxLength={30}/>
+                        <input type="tel" ref={telInput} placeholder="Seu Telefone (+258)" required name='tel' autoComplete='off' maxLength={9}/>
+                        <input type="text" ref={addressInput} placeholder='Seu Endereço' required name='address' autoComplete='off' maxLength={100}/>
                     </div>
                     <div className="form choice">
                         <select name="choice" required ref={selectRef}>
@@ -92,10 +144,10 @@ const Book = ({burgerID}:ChoiceIDType) => {
                             <option ref={hamburguer_duplo_acompanhado} value="hamburguer_duplo_acompanhado">Hambúrguer Duplo Acompanhado</option>
                             <option ref={batata_tomate_mayonaise} value="batata_tomate_mayonaise">Batata, Tomate e Mayonaise</option>
                         </select>
-                        <textarea cols={30} rows={8} placeholder="Observações" name="observations" autoComplete='off' maxLength={400}></textarea>
-                        <input type="hidden" name="_next" value="http://localhost:3000/#book"></input> 
-                        <input type="hidden" name="_next" value="https://burgershop.vercel.app/"></input>
-                        <button className='buy-btn'>Encomendar</button>
+                        <textarea ref={obsRef} cols={30} rows={8} placeholder="Observações" name="observations" autoComplete='off' maxLength={400}></textarea>
+                        {/* <input type="hidden" name="_next" value="http://localhost:3000/#book"></input> 
+                        <input type="hidden" name="_next" value="https://burgershop.vercel.app/"></input> */}
+                        <button className='buy-btn' onClick={book}>Encomendar</button>
                     </div>
                 </form>
             </div>
---
+++ _server/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+const mysql = require('mysql');
+const cors = require('cors');
+const path = require('path')
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname + "/public")));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT,() => {
+    console.log(PORT)
+})
+
+const db = mysql.createPool({
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    database: "thecornerdb"
+})
+
+app.post('/booking', (req, res) => {
+    const buyerName = req.body.buyerName;
+    const buyerTel = req.body.buyerTel;
+    const buyerAddress = req.body.buyerAddress;
+    const buyerChoice = req.body.buyerChoice;
+    const observations = req.body.observations;
+
+    const postQuery = "INSERT INTO `orders`(`buyerName`, `buyerTel`, `buyerAddress`, `buyerChoice`, `buyerObservations`) VALUES (?,?,?,?,?)"
+    db.query(postQuery, [buyerName, buyerTel, buyerAddress, buyerChoice, observations], (err, result) => {
+        res.send(result)
+    })
+})
+
